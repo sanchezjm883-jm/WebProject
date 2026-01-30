be6bda64c5a184c4090e14a6d3349e337c6644c2
sanchezjm883-jm <sanchezjm883@gmail.com>
2026-01-30 18:56:24 +0800

    Add files via upload

---
+++ Sanchez,John Mark..h
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BSIT_1D</title>
+</head>
+<body>
+    <h1>Header 6</h1>
+    <h6>Name:John Mark M. Sanchez</h6>
+    <h6>Age: 19</h6>
+    <h6>Address: Purok Calan1, Cordova, Cebu</h6>
+    <h6>Gender: Male</h6>
+    <h6>Description of yourself: I am a 1st year BSIT student at Cordova Public College. </h6>
+
+<h1>Paragraph</h1>
+<h6>Study Load: BSIT-1D</h6>
+<h6>PATHFIT 2- Exercise-Based Fitness Activities- 1:00-3:00 PM- CABALLERO</h6>
+<h6>WEB- Web Development- 8:00-9:00 AM- EMPACES</h6>
+<h6>DISCRETE 1- Discrete Structures- 10:00-11:00 AM- DINOY</h6>
+<h6>PROG 2- Computer Programming 2- 9:00-10:00 AM- EDA</h6>
+<h6>BASIC ELEC- Basic Electronics- 12:00-1:00 PM- COMAYAS</h6>
+<h6>GE 3- Reading in Philippine History- 9:00-4:00 PM- INOC</h6>
+<h6>FIL 2- Panitikan ng Pilipinas- 10:00-1:00 PM- MANCHING</h6>
+<h6>GE 4- Purposive Communication- 3:00-4:30 PM- BLASCO</h6>
+<h6>NSTP 2- National Service Training Program 2- 4:30-6:00 PM- PAQUIBOT</h6>
+
+</body>
+</html>
